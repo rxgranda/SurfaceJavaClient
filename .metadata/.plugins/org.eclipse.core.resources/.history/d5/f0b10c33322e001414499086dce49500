30269693d0832c137ce7add36603f0182c0ef06d
rxgranda <rxgq@hotmail.com>
2014-08-27 16:39:07 -0500

    Prueba Actualizar

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d5/f0b10c33322e001414499086dce49500
@@ -0,0 +1,5 @@
+package advanced.umleditor.logic;
+
+public class ObjetoUML {
+
+}
--- newMT4J/examples/advanced/umleditor/logic/ObjetoUML.java
+++ newMT4J/examples/advanced/umleditor/logic/ObjetoUML.java
@@ -1,5 +1,23 @@
 package advanced.umleditor.logic;
 
-public class ObjetoUML {
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public abstract class ObjetoUML {
+	private int idPersona;
+	private Timestamp tiempoInicio;
+	private Timestamp tiempoFin;
+	private ArrayList<Float> []puntos;
+	private static Calendar calendar = Calendar.getInstance();
+
+	
+	public ObjetoUML(){
+		tiempoInicio= new java.sql.Timestamp(calendar.getTime().getTime());
+		puntos=new ArrayList[2];
+	}
+	public void anadirPunto(float x,float y){
+		
+	}
 }
